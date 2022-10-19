5c35d059876c21a7efec4e555e021ad86cc8a355
MaralTach <yoloten.maral@gmail.com>
2022-10-19 23:52:49 +0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 window.onscroll = () =>{
     if(window.scrollY>80){
         document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+window.onload = () =>{
+    if(window.scrollY>80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
     }
 }
