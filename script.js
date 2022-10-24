1a698a422dcd4d6e1b6fe0f5d9b2ad8c1bf89dc4
MaralTach <yoloten.maral@gmail.com>
2022-10-24 23:22:53 +0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-searchForm = document.querySelector('.')
+searchForm = document.querySelector('.search-form');
 
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
 
 window.onscroll = () =>{
+
+    searchForm.classList.remove('active');
+
     if(window.scrollY>80){
         document.querySelector('.header .header-2').classList.add('active');
     }else{
